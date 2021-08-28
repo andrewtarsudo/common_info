379a72b3854b1b9d6fe1d1f9fd45898bba1f5b00
andrewtarsudo <62248471+andrewtarsudo@users.noreply.github.com>
2021-08-28 14:41:36 +0300

    Update renaming_files.py

--- python/renaming_files.py
+++ python/renaming_files.py
@@ -13,29 +13,29 @@ def change_directory(path_dir):
             path_new = pathlib.resolve(path)
             os.chdir(path_new)
             break
-        elif string(path) == "stop":
+        elif str(path) == "stop":
             print("You stopped work.")
             path_new = exception()
             break
         else:
             print("Path is incorrect.")
-            path_redef = string(input("Type the path to the directory"))
+            path_redef = str(input("Type the path to the directory"))
             path_dir = pathlib.PureWindowsPath(path_redef)
     
-    ans = string(path_new)
+    ans = str(path_new)
     return ans
 
 def change_name(path_orig, path_dest, filename, new_name):
     pathorig = pathlib.PureWindowsPath(path_orig)
     pathdest = pathlib.PureWindowsPath(path_dest)
-    file_old = pathlib.resolve(pathorig) / string(filename) + '.pdf'
-    file_new = pathlib.resolve(pathdest) / string(new_name) + '.pdf'
+    file_old = pathlib.resolve(pathorig) / str(filename) + '.pdf'
+    file_new = pathlib.resolve(pathdest) / str(new_name) + '.pdf'
     replaced_file = pathlib.Path(file_old).replace(file_new)
-    ans = string(pathlib.name(replaced_file))
+    ans = str(pathlib.name(replaced_file))
     return ans
 
 def find_text(filename, *argv):
-    path = pathlib.WindowsPath(pathlib.Path.cwd() / string(filename) + '.pdf')
+    path = pathlib.WindowsPath(pathlib.Path.cwd() / str(filename) + '.pdf')
     doc = fitz.open(path)
     text = ''
     search_number = []
@@ -45,15 +45,15 @@ def find_text(filename, *argv):
         i = 0
         for arg in argv:
             while True:
-                if text.find(string(arg), start) == -1:
+                if text.find(str(arg), start) == -1:
                     print("There are no substrings left")
                     break
                 else:
-                    index = text.find(string(arg), start)
+                    index = text.find(str(arg), start)
                     search_number[i] = text[index:index + length_num]
                     i += 1
                     start = index + length_num
     for unit in search_number:
-        ans = print(string(unit))
+        ans = print(str(unit))
     return search_number
 
