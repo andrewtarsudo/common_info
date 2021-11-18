48c05bd6156977d8764bf2758a1e2a6a8afbee31
andrewtarsudo <62248471+andrewtarsudo@users.noreply.github.com>
2021-11-18 09:28:13 +0300

    Update text_correction.py

--- python/module/text_correction.py
+++ python/module/text_correction.py
@@ -1,46 +1,45 @@
 import pathlib
 import check
 
-indeces = [5, 6, 7, 8, 9, 10, 12, 13, 14]
+INDECES = {5, 6, 7, 8, 9, 10, 12, 13, 14}
 
 
 def text_correction(path_file: str) -> str:
     path = pathlib.Path(path_file).resolve()
     name = list(pathlib.Path(path).name)
 
-    if len(pathlib.Path(path).name) < 15:
+    if len(name) < 15:
         print('It\'s too short!')
-        return str(name)
+        return ''.join(name)
 
-    if not str(name[4]) == '.':
+    if not name[4] == '.':
         name.insert(4, '.')
 
-    if not str(name[11]) == '.':
+    if not name[11] == '.':
         name.insert(11, '.')
 
-    for index in indeces:
-        if str(name[index]) == 'З':
-            name[index] = str('3')
+    for index in INDECES:
+        if name[index] == 'З':
+            name[index] = '3'
 
-        if str(name[index]) == 'О':
-            name[index] = str('0')
+        if name[index] == 'О':
+            name[index] = '0'
 
-    if len(name) > 16:
-        if str(name[-1:-3]) == str('СЬБ'):
-            del name[-2]
+    if len(name) > 16 and name[-1:-3] == 'СЬБ':
+        del name[-2]
 
-        if str(name[-1:-2]) == str('СЬ'):
-            name[-1] = str('Б')
+    if len(name) > 16 and name[-1:-2] == 'СЬ':
+        name[-1] = 'Б'
 
     # converse the list to the string
-    return str(name)
+    return ''.join(name)
 
 
 # text - the symbols extracted from the area of str() type
 # words_find - the list of searched symbols
 # output - the result of checks of bool() type
 # filter the text from the area to leave only needed part
-def text_filtering(text, words_find):
+def text_filtering(text: str, words_find: list) -> str:
     # index to extract the correct line
     index = -1
     for idx, value in enumerate(text.splitlines()):
