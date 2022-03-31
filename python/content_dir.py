3ca0fbd6f754a6dcd2d218ccd2666a906f18008b
andrewtarsudo <62248471+andrewtarsudo@users.noreply.github.com>
2022-04-01 02:26:51 +0300

    Update content_dir.py

--- python/content_dir.py
+++ python/content_dir.py
@@ -2,14 +2,23 @@ import pathlib
 
 
 def main():
-    pathfile = input('Path to the directory: ')
-    path = pathlib.Path(pathfile).resolve()
-
-    if pathlib.Path(path).is_dir():
-        for item in pathlib.Path(path).iterdir():
+    pathfile = input('Path to the directory:\n')
+    
+    try:
+        path = pathlib.Path(pathfile).resolve(strict=True)
+        for item in path.iterdir():
             print(item.name)
-    else:
-        print('Failed. Sorry')
+    except NotADirectoryError as e:
+        print(f"Error {e.errno} in line {e.__traceback__.tb_lineno}. It's not a directory.")
+    except FileNotFoundError as e:
+        print(f"Error {e.errno} in line {e.__traceback__.tb_lineno}. The specified directory doesn't exist.")
+    except TypeError as e:
+        print(f'TypeError in line {e.__traceback__.tb_lineno}. The path is incorrect.')
+    except PermissionError as e:
+        print(f'Error {e.errno} in line {e.__traceback__.tb_lineno}. You have no permission to access the directory.')
+    except OSError as e:
+        print(f'OSError {e.errno} in line {e.__traceback__.tb_lineno}.')
 
 
-main()
+if __name__ == '__main__':
+    main()
