78f35a6158c621cecbc1b7a0dcd31544211102b1
andrewtarsudo <62248471+andrewtarsudo@users.noreply.github.com>
2022-04-28 04:05:30 +0300

    Update yt_config.py

--- python/youtrack/yt_config.py
+++ python/youtrack/yt_config.py
@@ -10,12 +10,12 @@ class UserConfig:
     Define the configuration file to initiate YouTrack requests.
     
     Constants:
-        path --- the path to the JSON configuration file (default "./youtrack.json")\n
+        path --- the path to the JSON file (default "./youtrack.json")\n
         default_period_start --- the start period date (default the first day of the year)\n
         default_period_end --- the end period date (default the current day)\n
         attrs --- the attributes of the configuration file
         (default "login", "auth_token", "period_start", "period_end")\n
-        default_config --- the default configuration file values if the file does not exist.\n
+        default_config --- the default values if the file does not exist.\n
 
     Functions:
         func sys_login() -> str --- get the system name of the user\n
@@ -27,18 +27,17 @@ class UserConfig:
         check_json_attrs(file_path) --- validate all required attributes are set, otherwise, add the default values\n
         set_config_file(file_path) --- initiate all steps to get the proper configuration file
     """
-
     path = pathlib.Path("./youtrack.json")
 
     today = datetime.date.today()
     date_period_start = datetime.date(today.year, 1, 1)
     default_period_start = date_period_start.strftime("%Y-%m-%d")
     default_period_end = today.strftime("%Y-%m-%d")
-
+    # names to replace
     __dict_name_conv: dict[str, str] = {"mozglyakova": "matyushina", "AndrewTarasov": "tarasov-a"}
-
+    # attributes
     attrs = ("login", "auth_token", "period_start", "period_end")
-
+    # default configuration
     default_conf = {
         "login": "",
         "auth_token": "perm:dGFyYXNvdi1h.NjEtMTQw.1udDlV6zaAitHIgvw2eNQvF1sZ9JTZ",
@@ -49,7 +48,13 @@ class UserConfig:
     conf_values = dict()
 
     @staticmethod
-    def sys_login():
+    def sys_login() -> str:
+        """
+        Get the system user from the OS environment.
+
+        :return: the name of the user.
+        :rtype: str
+        """
         if os.uname().sysname == "Windows":
             key = "USERNAME"
         else:
@@ -58,16 +63,25 @@ class UserConfig:
         return sys_name if sys_name not in UserConfig.__dict_name_conv else UserConfig.__dict_name_conv[sys_name]
 
     @staticmethod
-    def check_json(file_path: pathlib.Path):
+    def check_json(file_path: pathlib.Path) -> bool:
+        """Verify if the file exists."""
         return file_path.exists()
 
     @staticmethod
     def generate_json(file_path: pathlib.Path):
+        """Create the file if it does not exist."""
         with open(file_path, "w") as json_file:
             json.dump(UserConfig.default_conf, json_file)
 
     @staticmethod
     def read_json(file_path: pathlib.Path) -> dict[str, str]:
+        """
+        Get the JSON file values.
+
+        :param pathlib.Path file_path: the path to the file
+        :return: the attribute dict values: login, auth_token, period_start, period_end.
+        :rtype: dict[str, str]
+        """
         with open(file_path, "r") as json_file:
             conf = json.load(json_file)
             for key, value in conf.items():
@@ -76,11 +90,12 @@ class UserConfig:
 
     @staticmethod
     def get_json_attr(key: str) -> Optional[str]:
-        __doc__ = """
-        Gets the Json file attribute if exists.
+        """
+        Get the JSON file attribute if it exists.
         
-        :param key: the attribute key to get, str
-        :return: the attribute value of the str type if the key is proper.
+        :param str key: the attribute key to get
+        :return: the attribute value or None
+        :rtype: str or None.
         """
         if key in UserConfig.conf_values:
             return UserConfig.conf_values[key]
@@ -90,6 +105,14 @@ class UserConfig:
 
     @staticmethod
     def update_json_item(file_path: pathlib.Path, key: str, value: str):
+        """
+        Modify the JSON file values.
+
+        :param pathlib.Path file_path: the path to the file
+        :param str key: the attribute name
+        :param str value: the attribute value
+        :return: None.
+        """
         if key not in UserConfig.conf_values:
             pass
         else:
@@ -99,6 +122,12 @@ class UserConfig:
 
     @staticmethod
     def check_json_attrs(file_path: pathlib.Path):
+        """
+        Verify that all main values are specified.
+
+        :param pathlib.Path file_path: the path to the file
+        :return: None.
+        """
         for key in UserConfig.attrs:
             if key not in UserConfig.conf_values:
                 UserConfig.conf_values[key] = UserConfig.default_conf[key]
@@ -109,16 +138,30 @@ class UserConfig:
 
     @staticmethod
     def set_config_file(file_path: pathlib.Path):
+        """
+        Define the UserConfig instance parameter values.
+
+        :param pathlib.Path file_path: the path to file
+        :return: the YouTrack configuration file.
+        :rtype: UserConfig
+        """
         if not UserConfig.check_json(file_path):
             UserConfig.generate_json(file_path)
+
         conf = UserConfig.read_json(file_path)
+
         for key, value in conf.items():
             UserConfig.conf_values[key] = value
+
         name = UserConfig.get_json_attr("login")
+
         if not name or name in UserConfig.__dict_name_conv:
             login = UserConfig.sys_login()
             UserConfig.update_json_item(file_path, "login", login)
         UserConfig.check_json_attrs(file_path)
+        user_config = UserConfig()
+
+        return user_config
 
 
 def main():
