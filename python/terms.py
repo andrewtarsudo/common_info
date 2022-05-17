d2f834537fd8d318479393230367b4b56f0a128c
andrewtarsudo <62248471+andrewtarsudo@users.noreply.github.com>
2022-05-17 11:29:58 +0300

    Update terms.py

--- python/terms.py
+++ python/terms.py
@@ -1,10 +1,8 @@
 import sqlite3
-import random
 from bisect import bisect_left
 import logging
-
-
-logging.basicConfig(filename="terms.log")
+from typing import Optional
+from collections import Counter
 
 
 class Const:
@@ -14,7 +12,6 @@ class Const:
 def binary_search(values: list[str], search_value: str, min_index: int = 0, max_index: int = None):
     if max_index is None:
         max_index = len(values)
-    
     position = bisect_left(values, search_value, min_index, max_index)
     return position if position != max_index and values[position] == search_value else -1
 
@@ -29,6 +26,7 @@ class Term:
         self.identifier = Term.index
 
         Term.index += 1
+        Const.dict_short_id[self.identifier] = self.short
 
     def __format__(self, format_spec):
         return f"{self.short}\t{self.full}, {self.meaning_rus}"
@@ -88,6 +86,10 @@ class Term:
             return self.short >= other.short
 
 
+def add_term(short: str, full: str, meaning_rus: str = None):
+    return Term(short, full, meaning_rus)
+
+
 class TermList:
     def __init__(self, name: str, terms: list[Term] = None):
         if terms is None:
@@ -96,23 +98,57 @@ class TermList:
         self.terms = terms
         self.dict_short_id = dict()
 
-    def _identifiers(self):
+    def _identifiers(self) -> list[int]:
         return [term.identifier for term in self.terms]
 
-    def _shorts(self):
+    def _shorts(self) -> list[str]:
         return [term.short for term in self.terms]
 
-    def _rus(self):
+    def _rus(self) -> list[str]:
         return [term.meaning_rus for term in self.terms]
 
-    def binary_term_id(self, short: str):
+    def binary_by_short(self, short: str) -> Optional[Term]:
         position = binary_search(self._shorts(), short)
         if position != -1:
             return self.terms[position]
         else:
             logging.warning(f"The term {short} is not in the dictionary.")
             return None
-    
-    
-    
-    
+
+    def __short_same_keys(self):
+        cntr = Counter(self.dict_short_id.values())
+        counter = +cntr
+        return [item for item in counter.keys() if counter[item] > 1]
+
+    def __short_same_values(self, item: str):
+        list_identifiers = []
+        list_shorts = copy(self._shorts())
+        return [identifier for identifier in self.dict_short_id if self.dict_short_id[identifier] == item]
+
+    def dict_short_id_same(self):
+        dict_same = dict()
+        for item in self.__short_same_keys():
+            dict_same[item] = self.__short_same_values(item)
+        return dict_same
+
+    def search_short(self, short: str):
+        if short in self.dict_short_id_same():
+            return self.dict_short_id_same()[short]
+        else:
+            return self.binary_by_short(short)
+
+
+def main():
+    # filename = "terms_log.log"
+    # fmt = "%(levelName)s %(asctime)s, %(funcName) --- %(message)s"
+    # level = logging.INFO
+    # logging.basicConfig(filename=filename, format=fmt, level=level)
+    list_values = ["a", "b", "c", "a", "a", "c"]
+    cntr = Counter(list_values)
+    counter = +cntr
+    values = [item for item in counter.keys() if counter[item] > 1]
+    print(values)
+
+
+if __name__ == "__main__":
+    main()
