117069346826dc5bb1d8c4c54757a3e52c7de555
andrewtarsudo <62248471+andrewtarsudo@users.noreply.github.com>
2022-05-23 07:40:30 +0300

    Update main.py

--- python/youtrack/main.py
+++ python/youtrack/main.py
@@ -1,183 +1,142 @@
-from typing import Optional, Union
+from typing import Optional
 import openpyxl
 from openpyxl.cell.cell import Cell
+from openpyxl.workbook.workbook import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
 from yt_config import UserConfig
 from yt_requests import User, Issue, IssueWorkItem, _IssueMerged
 from xl_table import ExcelProp, PyXLRow, PyXLWorkItem, _PyXLMerged
-from _style_work_item import _StyleWorkItem, _StyleWorkItemList
+from _style_work_item import _StyleWorkItemList
+import datetime
 
 
-def compare_merged_issue(issue_merged: _IssueMerged, pyxl_merged: _PyXLMerged):
+def separate_issues(excel_prop: ExcelProp, user: User) -> dict[str, list[str]]:
     """
-    Get the unequal issues and work items if any.
+    Separate issues depending on the following actions with them.
+
+    :param excel_prop: the ExcelProp instance
+    :param user: the User instance
+    :return: the dictionary of states and issue names.
+    :rtype: dict[str, list[str]]
+    """
+    dict_issue_names: dict[str, list[str]] = dict()
+    # issue names
+    xl_names = set(excel_prop.pyxl_row_names())
+    yt_names = set(user.issue_names())
+    # issues from the tables left unchanged
+    dict_issue_names["xl_names_not_changed"] = [*xl_names.difference(yt_names)]
+    # issues from the YouTrack to add to the table
+    dict_issue_names["yt_names_add"] = [*yt_names.difference(xl_names)]
+    # issues that changed
+    dict_issue_names["names_changed"] = [*set.intersection(xl_names, yt_names)]
+    return dict_issue_names
+
+
+def _convert_to_pyxl_row(issue_merged: _IssueMerged, excel_prop: ExcelProp) -> PyXLRow:
+    """
+    Generate the task in the table.
+
+    :param _IssueMerged issue_merged: the _IssueMerged instance
+    :param ExcelProp excel_prop: the ExcelProp instance
+    :return: the new task.
+    :rtype: PyXLRow
+    """
+    issue, state, summary, parent, deadline = issue_merged.issue_item.to_tuple()
+    # get the new row
+    row: int = excel_prop.add_row(state)
+    # add the row
+    excel_prop.ws.insert_rows(row)
+    # parse the values to the new table row
+    excel_prop.ws[f"B{row}"].value = parent
+    excel_prop.ws[f"C{row}"].value = issue
+    excel_prop.ws[f"D{row}"].value = summary
+    excel_prop.ws[f"E{row}"].value = deadline
+    # set the base cell
+    cell: Cell = excel_prop.ws[f"C{row}"]
+    return PyXLRow(excel_prop, cell)
+
+
+def _convert_to_pyxl_work_items(issue_merged: _IssueMerged, excel_prop: ExcelProp) -> list[PyXLWorkItem]:
+    """
+    Generate the work items in the table.
+
+    :param _IssueMerged issue_merged: the _IssueMerged instance
+    :param ExcelProp excel_prop: the ExcelProp instance
+    :return: the new work items.
+    :rtype: list[PyXLWorkItem]
+    """
+    work_items = []
+    for work_item in issue_merged.work_items:
+        date: datetime.date = work_item.date
+        column: str = excel_prop.get_column_date(date)
+        row: int = excel_prop.add_row(issue_merged.issue_item.state)
+        cell: Cell = excel_prop.ws[f"{column}{row}"]
+        work_items.append(PyXLWorkItem(excel_prop, cell))
+    return work_items
+
+
+def convert_to_xl_merged(issue_merged: _IssueMerged, excel_prop: ExcelProp):
+    """
+    Generate the _PyXLMerged in the table.
+
+    :param _IssueMerged issue_merged: the _IssueMerged instance
+    :param ExcelProp excel_prop: the ExcelProp instance
+    :return: None.
+    """
+    work_items = _convert_to_pyxl_work_items(issue_merged, excel_prop)
+    pyxl_row = _convert_to_pyxl_row(issue_merged, excel_prop)
+    cell = pyxl_row.issue
+    pyxl_merged = _PyXLMerged(excel_prop, cell)
+    for work_item in work_items:
+        work_item.set_style()
+
+
+def compare_merged_issue(issue_merged: _IssueMerged, pyxl_merged: _PyXLMerged) -> Optional[Issue]:
+    """
+    Get the unequal issue if any.
 
     :param _IssueMerged issue_merged: the instances from the YouTrack
     :param _PyXLMerged pyxl_merged: the instances from the table
     :return: the instances from the YouTrack to add or modify in the table.
     :rtype: None or Issue
     """
-    issue: Issue = issue_merged.issue_item
-    pyxl_row: PyXLRow = pyxl_merged.pyxl_row
-    return None if issue.to_tuple() == pyxl_row.to_tuple() else issue
+    return None if issue_merged.issue_item.to_tuple() == pyxl_merged.pyxl_row.to_tuple() else issue_merged.issue_item
 
 
-def compare_merged_work_item(issue_merged: _IssueMerged, pyxl_merged: _PyXLMerged):
+def compare_merged_work_item(issue_merged: _IssueMerged, pyxl_merged: _PyXLMerged) -> Optional[list[IssueWorkItem]]:
     """
-    Get the unequal issues and work items if any.
+    Get the unequal work items if any.
 
     :param _IssueMerged issue_merged: the instances from the YouTrack
     :param _PyXLMerged pyxl_merged: the instances from the table
     :return: the instances from the YouTrack to add or modify in the table.
     :rtype: None or list[IssueWorkItem]
     """
-    issue_work_items: list[IssueWorkItem] = issue_merged.work_items
-    pyxl_items: list[PyXLWorkItem] = pyxl_merged.work_items
-    return [work_unit for work_unit in issue_work_items if
-            not any(pyxl_item.to_tuple() == work_unit.to_tuple() for pyxl_item in pyxl_items)]
+    # issue, date, spent_time
+    return [work_item for work_item in issue_merged.work_items
+            if work_item.to_tuple() not in pyxl_merged.items_to_tuple()]
 
 
-def create_pyxl_row(issue: Issue, excel_prop: ExcelProp) -> PyXLRow:
+def get_modified_attr_pyxl_row(issue_merged: _IssueMerged, pyxl_merged: _PyXLMerged) -> list[tuple]:
     """
-    Create the PyXLRow instance.
+    Get the PyXLRow attributes and values to modify.
 
-    :param Issue issue: the Issue instance
-    :param ExcelProp excel_prop: the ExcelProp instance
-    :return: the PyXLRow instance.
-    :rtype: PyXLRow
+    :param _IssueMerged issue_merged: the instances from the YouTrack
+    :param _PyXLMerged pyxl_merged: the instances from the table
+    :return: the parameters to modify.
+    :rtype: list[tuple[str, Any]]
     """
-    state = issue.state
-    row = max(excel_prop.list_state_item(state)) + 1
-    excel_prop.ws.insert_rows(row)
-    excel_prop.ws[f"B{row}"].value = issue.parent
-    excel_prop.ws[f"C{row}"].value = issue.issue
-    excel_prop.ws[f"D{row}"].value = issue.summary
-    excel_prop.ws[f"E{row}"].value = issue.deadline
-    return PyXLRow(excel_prop, excel_prop.ws[f"C{row}"])
+    attrs = "state", "summary", "parent", "deadline"
+    return [(attr, getattr(issue_merged, attr)) for attr in attrs
+            if getattr(issue_merged, attr) != getattr(pyxl_merged, attr)]
 
 
-def issue_yt_name(user: User, issue_name: str) -> Issue:
-    """
-    Get the Issue instance by its name.
-
-    :param User user: the User instance
-    :param str issue_name: the issue name
-    :return: the Issue instance.
-    :rtype: Issue
-    """
-    issue: Issue
-    for issue in user.dict_issue.values():
-        if issue.issue == issue_name:
-            return issue
-        else:
-            continue
+def merge_issues(issue_name: str, user: User, excel_prop: ExcelProp) -> tuple[_IssueMerged, _PyXLMerged]:
+    return user.dict_issue_merged[issue_name], excel_prop.dict_pyxl_merged[issue_name]
 
 
-def issue_xl_name(excel_prop: ExcelProp, issue_name: str) -> PyXLRow:
-    """
-    Get the PyXLRow instance by its name.
-
-    :param ExcelProp excel_prop: the ExcelProp instance
-    :param str issue_name: the issue name
-    :return: the PyXLRow instance.
-    :rtype: PyXLRow
-    """
-    issue: PyXLRow
-    for issue in excel_prop.dict_pyxl_row.values():
-        if issue.issue == issue_name:
-            return issue
-        else:
-            continue
-
-
-def get_row(excel_prop: ExcelProp, issue_name: str) -> Optional[int]:
-    """
-    Specify the row of the issue.
-
-    :param ExcelProp excel_prop: the ExcelProp instance
-    :param str issue_name: the issue name
-    :return: the issue row.
-    :rtype: int
-    """
-    issue: PyXLRow
-    if not any(issue.issue == issue_name for issue in excel_prop.dict_pyxl_row.values()):
-        return None
-    else:
-        return issue_xl_name(excel_prop, issue_name).row
-
-
-def get_row_new_state(user: User, excel_prop: ExcelProp, issue_name: str) -> int:
-    """
-    Specify the row for the new issue.
-
-    :param User user: the User instance
-    :param ExcelProp excel_prop: the ExcelProp instance
-    :param str issue_name: the issue name
-    :return: the row of the new issue.
-    :rtype: int
-    """
-    state = issue_yt_name(user, issue_name).state
-    row = max(excel_prop.list_state_item(state)) + 1
-    return row
-
-
-def create_pyxl_work_item(user: User, issue_work_item: IssueWorkItem, excel_prop: ExcelProp) -> PyXLWorkItem:
-    """
-    Create the PyXLWorkItem instance.
-
-    :param User user: the User instance
-    :param IssueWorkItem issue_work_item: the IssueWorkItem instance
-    :param ExcelProp excel_prop: the ExcelProp instance
-    :return: the PyXLWorkItem instance.
-    :rtype: PyXLWorkItem
-    """
-    issue, date, spent_time = issue_work_item.issue, issue_work_item.date, issue_work_item.spent_time
-    # the cell column
-    column = excel_prop.get_column_date(date)
-    # if the issue name is already in the table
-    if get_row(excel_prop, issue) is not None:
-        row = get_row(excel_prop, issue)
-    # if the issue name is not in the table
-    else:
-        row = get_row_new_state(user, excel_prop, issue)
-        excel_prop.ws.insert_rows(row)
-    # define the cell
-    cell: Cell = excel_prop.ws[f"{column}{row}"]
-    return PyXLWorkItem(excel_prop, cell, cell.style.name)
-
-
-def pair_merged(
-        issue_merged: list[_IssueMerged],
-        pyxl_merged: list[_PyXLMerged]) -> dict[str, tuple[_IssueMerged, _PyXLMerged]]:
-    """
-    Match the _Merged instances.
-
-    :param issue_merged: the _IssueMerged instances
-    :type issue_merged: list[_IssueMerged]
-    :param pyxl_merged: the _PyXLMerged instances
-    :type issue_merged: list[_PyXLMerged]
-    :return: the dictionary of the paired instances.
-    """
-    yt_pyxl_paired: dict[str, tuple[_IssueMerged, _PyXLMerged]] = dict()
-    for issue in issue_merged:
-        for pyxl in pyxl_merged:
-            if pyxl.issue_name == issue.issue_name:
-                yt_pyxl_paired[issue.issue_name] = (issue, pyxl)
-    return yt_pyxl_paired
-
-
-def unpaired(issue_merged: list[_IssueMerged], pyxl_merged: list[_PyXLMerged]) -> list[str]:
-    """
-    Get the unpaired YouTrack issue names.
-
-    :param issue_merged: the _IssueMerged instances
-    :type issue_merged: list[_IssueMerged]
-    :param pyxl_merged: the _PyXLMerged instances
-    :type issue_merged: list[_PyXLMerged]
-    :return: the unpaired issue names.
-    :rtype: list[str]
-    """
-    return [issue.issue_name for issue in issue_merged
-            if not any(pyxl.issue_name == issue.issue_name for pyxl in pyxl_merged)]
+def modify_issues():
+    pass
 
 
 def main():
@@ -185,14 +144,52 @@ def main():
     user = User(youtrack_config)
     user.get_issue_work_items()
     user.get_issues()
-    issue_merged = user.get_merged()
+    user.get_current_issues()
+    yt_issues_merged = user.get_merged()
 
     path = youtrack_config.get_json_attr("path_table")
     wb = openpyxl.load_workbook(path)
-    ws = wb["12 мес."]
+    ws: Worksheet = wb["12 мес."]
     style_list = _StyleWorkItemList("styles")
     excel_prop = ExcelProp(ws, "excel_prop", style_list)
-    pyxl_merged = excel_prop.get_merged()
+    pyxl_issues_merged = excel_prop.get_merged()
+
+    dict_issues = separate_issues(excel_prop, user)
+
+    for issue_name in dict_issues["yt_names_add"]:
+        yt_issue_merged = user.dict_issue_merged[issue_name]
+        convert_to_xl_merged(yt_issue_merged, excel_prop)
+
+    issue_modified = []
+    work_item_modified = []
+
+    for issue_name in dict_issues["names_changed"]:
+        yt_issue_merged, pyxl_issue_merged = merge_issues(issue_name, user, excel_prop)
+
+        issue_compared = compare_merged_issue(yt_issue_merged, pyxl_issue_merged)
+        if issue_compared is not None:
+            issue_modified.append(issue_compared.issue)
+
+        work_item_compared = compare_merged_work_item(yt_issue_merged, pyxl_issue_merged)
+        if work_item_compared is not None:
+            for item in work_item_compared:
+                work_item_modified.append(item)
+
+    for issue_name in issue_modified:
+        yt_issue_merged, pyxl_issue_merged = merge_issues(issue_name, user, excel_prop)
+        attrs = get_modified_attr_pyxl_row(yt_issue_merged, pyxl_issue_merged)
+        for attr, attr_value in attrs:
+            if attr == "state":
+                pyxl_issue_merged.modify_pyxl_state(attr_value)
+            else:
+                column = excel_prop.dict_attr_column[attr]
+                row = pyxl_issue_merged.row
+                excel_prop.ws[f"{column}{row}"].value = attr_value
+
+    for work_item in work_item_modified:
+        excel_prop.add_work_item(work_item.issue, work_item.date, work_item.spent_time)
+    print("The work is finished.")
+    wb.save(f"copy_{user.path_table}")
 
 
 if __name__ == "__main__":
